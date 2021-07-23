30e911b2ac9b7c117dbfa23531ff046cb9c0d20d
Vitor Peixoto <vitorgslpeixoto@gmail.com>
2021-07-22 22:28:47 -0300

    Exercício 3: mostrar a "intro" ao clicar1~

--- starwars.js
+++ starwars.js
@@ -6,6 +6,7 @@
 import { play } from './music.js';
 import { friendlyFetch } from './friendly-fetch.js';
 import { toRoman } from './roman.js';
+import { restartAnimation } from './restart-animation.js';
 
 const API_ENDPOINT = 'https://swapi.dev/api'
 
@@ -16,25 +17,41 @@ const musicData = {
     artist: "John Williams"
 }
 
+// Elementos estáticos da página
+const movieListElement = document.querySelector('#filmes ul');
+const movieIntroElement = document.querySelector('pre.introducao');
+
+const handleEpisodeClick = (movie) => {
+    const content = `Episode ${toRoman(movie.episode_id)}
+    ${movie.title}
+    
+    ${movie.opening_crawl}
+    `;
+
+    movieIntroElement.innerHTML = content;
+    restartAnimation(movieIntroElement);
+}
+
+const insereFilmeNaPagina = (movie) => {
+    const movieElement = document.createElement('li')
+
+    const episodeNumber = toRoman(movie.episode_id);
+    const title = `Episode ${episodeNumber} - ${movie.title}`;
+
+    movieElement.addEventListener('click', () => {
+        handleEpisodeClick(movie);
+    })
+
+    const textElement = document.createElement('p');
+    textElement.innerHTML = title;
+
+    movieElement.appendChild(textElement);
+    movieListElement.appendChild(movieElement);
+}
+
 const main = async () => {
     const filmsResponse = await friendlyFetch(`${API_ENDPOINT}/films`);
-    const movies = filmsResponse.results;
-
-    // Elementos estáticos da página
-    const movieListElement = document.querySelector('#filmes ul');
-
-    const insereFilmeNaPagina = (movie) => {
-        const movieElement = document.createElement('li')
-
-        const episodeNumber = toRoman(movie.episode_id);
-        const title = `Episode ${episodeNumber} - ${movie.title}`;
-
-        const textElement = document.createElement('p');
-        textElement.innerHTML = title;
-
-        movieElement.appendChild(textElement);
-        movieListElement.appendChild(movieElement);
-    }
+    const movies = filmsResponse.results;    
 
     movieListElement.innerHTML = '';
     Object.values(movies).forEach( insereFilmeNaPagina );
