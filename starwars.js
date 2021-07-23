f03854c54389221191013590046ef392ad192976
Vitor Peixoto <vitorgslpeixoto@gmail.com>
2021-07-22 21:52:03 -0300

    Exercício 1: player de música

--- starwars.js
+++ starwars.js
@@ -3,5 +3,15 @@
 // para fazer uma requisição assíncrona e:
 //  - Pegar a lista de filmes (AJAX) e preencher no HTML
 //  - Quando um filme for clicado, exibir sua introdução
+import { play } from './music.js';
 
 const API_ENDPOINT = 'https://swapi.dev/api'
+
+const musicData = {
+    audioUrl: "audio/tema-sw.mp3",
+    coverImageUrl: "imgs/logo.svg",
+    title: "intro",
+    artist: "John Williams"
+}
+
+play(musicData, document.body);
